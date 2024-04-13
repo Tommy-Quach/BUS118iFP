235ef0e048f999a5b75a5b1f70afc296f0561b88
Edward Shen <edwardshen24@email.com>
2024-04-12 20:49:39 -0700

    .

--- pages/page_1.py
+++ pages/page_1.py
@@ -18,7 +18,7 @@ response = client.chat.completions.create(
         {
           "type": "image_url",
           "image_url": {
-            "url": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Gfp-wisconsin-madison-the-nature-boardwalk.jpg/2560px-Gfp-wisconsin-madison-the-nature-boardwalk.jpg",
+            "url": "",
           },
         },
       ],
