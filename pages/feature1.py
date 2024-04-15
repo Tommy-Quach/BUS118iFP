378b45f7d59eaff438ba4ccab5a89a3f44767b7b
Edward Shen <edwardshen24@gmail.com>
2024-04-14 22:27:45 -0700

    feature 1

--- pages/feature1.py
+++ pages/feature1.py
@@ -1,34 +1,27 @@
-﻿import os
+﻿from openai import OpenAI
+import os
 import openai
-import streamlit as st
-from openai import OpenAI
-
-
-st.markdown("# Page 1: Text Generation And Analysis")
-st.divider()
-st.sidebar.markdown("# Page 1: Text Generation")
-
+ 
 openai.api_key = os.environ["OPENAI_API_KEY"]
-
 client = OpenAI()
 
-# create a wrapper function
-def get_completion(prompt, model="gpt-3.5-turbo"):
-   completion = client.chat.completions.create(
-        model=model,
-        messages=[
-        {"role":"system",
-         "content": "Your job is to analyze text based on user input and educate users on potholes."},
-        {"role": "user",
-         "content": prompt},
-        ]
-    )
-   return completion.choices[0].message.content
+response = client.chat.completions.create(
+  model="gpt-4-turbo",
+  messages=[
+    {
+      "role": "user",
+      "content": [
+        {"type": "text", "text": "What's in this image?"},
+        {
+          "type": "image_url",
+          "image_url": {
+            "url": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Gfp-wisconsin-madison-the-nature-boardwalk.jpg/2560px-Gfp-wisconsin-madison-the-nature-boardwalk.jpg",
+          },
+        },
+      ],
+    }
+  ],
+  max_tokens=300,
+)
 
-
-with st.form(key = "chat"):
-    prompt = st.text_input("What would you like to know about potholes or do you have any potholes from a news article you'd like to add to the map?: ") 
-    submitted = st.form_submit_button("Submit")
-    
-    if submitted:
-        st.write(get_completion(prompt))
+print(response.choices[0])
