35da2fa07a2921d3c489223d52418909fbd8bd2e
Edward Shen <edwardshen24@email.com>
2024-04-16 11:37:08 -0700

    pages

--- pages/feature1.py
+++ pages/feature1.py
@@ -2,20 +2,19 @@
 import os
 import openai
  
-openai.api_key = os.environ["OPENAI_API_KEY"]
+openai.api_key = os.environ["<OPENAI_API_KEY>"]
 client = OpenAI()
-
 response = client.chat.completions.create(
   model="gpt-4-turbo",
   messages=[
     {
       "role": "user",
       "content": [
-        {"type": "text", "text": "Focus on the damaged roads and potholes of the image. Assess the severity of damage include size and depth."},
+        {"type": "text", "text": "What' in this image?"},
         {
           "type": "image_url",
           "image_url": {
-            "url": "https://media.istockphoto.com/id/174662203/photo/pot-hole.jpg?s=612x612&w=0&k=20&c=HhFYQD5qAJItGzYWJJQ72nxBR8iidL7Np2g82dfvnoM=",
+            "url": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Gfp-wisconsin-madison-the-nature-boardwalk.jpg/2560px-Gfp-wisconsin-madison-the-nature-boardwalk.jpg",
           },
         },
       ],
