28abfde766891f2e0cfaaccefb7cd59a0ea2ae07
Edward Shen <edwardshen24@email.com>
2024-04-16 15:19:14 -0700

    allows multiple files to be uploaded

--- pages/feature3.py
+++ pages/feature3.py
@@ -4,15 +4,17 @@ from PIL import Image
 def main():
     st.title("Image Uploader")
 
-    uploaded_image = st.file_uploader("Choose an image...", type=["jpg", "mp4", "jpeg", "png"])
-
-    if uploaded_image is not None:
-        image = Image.open(uploaded_image)
-        st.image(image, caption="Uploaded Image", use_column_width=True)
+    uploaded_images = st.file_uploader("Choose image(s) to upload...", accept_multiple_files=True, type=["jpg", "mp4", "jpeg", "png"])
+ 
+    if uploaded_images is not None:
+        for uploaded_image in uploaded_images:
+            image = Image.open(uploaded_image)
+            st.image(image, caption="Uploaded Image", use_column_width=True)
 
     st.markdown("---")
     st.write("Disclaimer: This site may collect metadata from uploaded images, including location data.")
     st.write("Please be cautious while uploading images containing sensitive information.")
+
 
 if __name__ == "__main__":
     main()
